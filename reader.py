95d44d946c46e3133aed8836be0cf8a8caee5b9f
Alejandro Reyna <AleejandroReyna@gmail.com>
2025-04-12 00:00:41 -0600

    added template with form and content

--- reader.py
+++ reader.py
@@ -1,15 +1,22 @@
 from docx import Document
+from flask import Flask, render_template
 
 def read_doc():
   doc = Document('./test.docx')
 
   highlited = []
+  paragraphs = doc.paragraphs
   for para in doc.paragraphs:
     for run in para.runs:
       if run.font.highlight_color:
         highlited.append(run.text)
 
-  return highlited
+  return (highlited, paragraphs)
 
-if __name__ == '__main__':
-  print(read_doc())
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/', methods=['GET'])
+def index():
+  (highlited, paragraphs ) = read_doc()
+  return render_template('edit.html', items=highlited, doc=paragraphs)
